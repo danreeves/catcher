da28ec0f12301411c0d6665d3f39705c5705eb6b
Dan Reeves <heydanreeves@gmail.com>
2015-06-17 15:51:07 +0100

    stuff

--- index.js
+++ index.js
@@ -1,17 +1,33 @@
 var Hapi = require('hapi');
+var fs = require('graceful-fs');
+var IS_LOCAL = (process.argv.slice(2).indexOf('--local') > -1);
+
+var log = fs.createWriteStream('log.txt', {'flags': 'a'});
+log.write('Line\tMessage\tURL\n');
 
 var server = new Hapi.Server();
 
 server.connection({
-    port: 8000
+    port: (IS_LOCAL) ? 8080 : 80,
+    routes: {
+        cors: true
+    }
 });
 
 server.route({
     method: 'GET',
     path:'/',
     handler: function (request, reply) {
-       reply('HAPI');
+        var line = '';
+        Object.keys(request.query).forEach(function (key) {
+            line += request.query[key] + '\t';
+        });
+        line += '\n';
+        log.write(line);
+        reply();
     }
 });
 
-server.start();
+server.start(function () {
+    console.log('Server running at:', server.info.uri);
+});
